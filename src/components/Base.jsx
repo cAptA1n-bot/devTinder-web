4c0f1e70debba3d388c3c8b00886890232de5b8a
Prince Yadav <prince1bot001@gmail.com>
2026-03-03 23:34:41 +0530

    loads the user on first mount

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavBar from './NavBar'
 import { Outlet } from 'react-router-dom'
 import Footer from './Footer'
+import axios from 'axios'
+import { BASE_URL } from '../utils/constants'
+import { useDispatch } from 'react-redux'
+import { addUser } from '../utils/userSlice'
 
 const Base = () => {
+  const dispatch = useDispatch();
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile", { withCredentials: true })
+      dispatch(addUser(res.data))
+    }
+    catch (err) {
+      console.error(err);
+    }
+  }
+  useEffect(() => {
+    fetchUser();
+  }, [])
+
+
   return (
     <div>
-      <NavBar/>
-      <Outlet/>
-      <Footer/>
+      <NavBar />
+      <Outlet />
+      <Footer />
     </div>
   )
 }
