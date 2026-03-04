604aa7e0e1268680616b5c3d6c2d8b8723253919
Prince Yadav <prince1bot001@gmail.com>
2026-03-04 10:51:51 +0530

    basic navigation done

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -1,25 +1,33 @@
 import React, { useEffect } from 'react'
 import NavBar from './NavBar'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 import Footer from './Footer'
 import axios from 'axios'
 import { BASE_URL } from '../utils/constants'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { addUser } from '../utils/userSlice'
 
 const Base = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
   const fetchUser = async () => {
     try {
       const res = await axios.get(BASE_URL + "/profile", { withCredentials: true })
       dispatch(addUser(res.data))
     }
     catch (err) {
+      if(err.response?.status === 401){
+        navigate("/login");
+      }
       console.error(err);
     }
   }
   useEffect(() => {
-    fetchUser();
+    if(userData) return;
+
+      fetchUser();
+    
   }, [])
 
 
