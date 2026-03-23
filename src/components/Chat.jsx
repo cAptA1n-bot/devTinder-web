6dd212b4a703551a87a566853465ed19efb11895
Prince Yadav <prince1bot001@gmail.com>
2026-03-23 22:44:15 +0530

    updated ui for chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -11,8 +11,11 @@ const Chat = () => {
                     <div className="chat-bubble">You were the Chosen One!</div>
                     <div className="chat-footer opacity-50">Seen</div>
                 </div>
-            
-                <input type="text" className="w-full lg:w-175 fixed bottom-15 border border-gray-300 p-4 h-20" placeholder="message..." />
+            <div className="fixed lg:w-175 bottom-15 flex gap-0.5 justify-center">
+                <input type="text" className="w-84 lg:w-150 border border-gray-300 p-4 h-16 rounded-lg text-[18px] lg:mb-1" placeholder="message..." />
+                <button className="btn btn-secondary w-23 h-16">Send</button>
+            </div>
+                
             
         </div>
     )
