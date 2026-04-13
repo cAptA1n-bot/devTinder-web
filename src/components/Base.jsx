cec6c37cdf3f2a535e5bc63c72c9771906581e0d
Prince Yadav <prince1bot001@gmail.com>
2026-04-13 17:49:05 +0530

    bug fixes

--- src/components/Base.jsx
+++ src/components/Base.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react'
 import NavBar from './NavBar'
 import { Outlet, useNavigate } from 'react-router-dom'
-import Footer from './Footer'
 import axios from 'axios'
 import { BASE_URL } from '../utils/constants'
 import { useDispatch, useSelector } from 'react-redux'
